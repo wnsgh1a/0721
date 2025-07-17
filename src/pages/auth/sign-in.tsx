fde323bac41994679e884ec61667bad6d876cd96
9Diin <weartstudio@naver.com>
2025-07-17 15:34:41 +0900

    🍀 [feature] 로그인시, Validation Check 기능 구현 및 Supabase 로그인 기능 연동

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,41 @@
+import { Link } from "react-router";
 import { AppFooter, AppHeader } from "@/components/common";
-import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, Input, Label } from "@/components/ui";
+import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Input, Label } from "@/components/ui";
+import supabase from "@/lib/supabase";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
+const formSchema = z.object({
+    email: z.email("올바른 이메일을 입력해주세요."),
+    password: z.string().min(8, {
+        message: "비밀번호는 최소 8자 이상이어야 합니다.",
+    }),
+});
 
 function SignIn() {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const handleToggle = () => setShowPassword((prev) => !prev);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    // Supabase를 활용한 로그인 기능 구현
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: values.email,
+            password: values.password,
+        });
+    };
+
     return (
         <div className="page">
             <AppHeader />
@@ -23,31 +57,58 @@ function SignIn() {
                             <CardDescription>로그인을 위한 정보를 입력하세요.</CardDescription>
                         </CardHeader>
                         <CardContent className="p-0 sm:px-6">
-                            <form>
-                                <div className="flex flex-col gap-6">
-                                    <div className="grid gap-2">
-                                        <Label htmlFor="email">이메일</Label>
-                                        <Input id="email" type="email" placeholder="이메일을 입력하세요." required />
-                                    </div>
-                                    <div className="grid gap-2">
-                                        <div className="flex items-center">
-                                            <Label htmlFor="password">비밀번호</Label>
-                                            <a href="#" className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-                                                비밀번호를 잊으셨나요?
-                                            </a>
-                                        </div>
-                                        <Input id="password" type="password" placeholder="비밀번호를 입력하세요." required />
-                                    </div>
-                                </div>
-                            </form>
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>이메일</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="이메일을 입력하세요." {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem className="relative">
+                                                <div className="w-full flex items-center justify-between">
+                                                    <FormLabel>비밀번호</FormLabel>
+                                                    <Link to={"/sign-in/credentials"} className="text-sm underline">
+                                                        비밀번호를 잊으셨나요?
+                                                    </Link>
+                                                </div>
+                                                <FormControl>
+                                                    <Input type={showPassword ? "text" : "password"} placeholder="비밀번호를 입력하세요." {...field} />
+                                                </FormControl>
+                                                <Button type="button" size={"icon"} className="absolute top-7 right-1 bg-transparent hover:bg-transparent" onClick={handleToggle}>
+                                                    {showPassword ? <Eye className="text-muted-foreground" /> : <EyeOff className="text-muted-foreground" />}
+                                                </Button>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <Button type="submit" variant={"destructive"} className="w-full mt-3 -mb-4">
+                                        로그인
+                                    </Button>
+                                </form>
+                            </Form>
                         </CardContent>
                         <CardFooter className="flex-col gap-2 px-0 sm:px-6">
-                            <Button type="submit" variant={"destructive"} className="w-full">
-                                로그인
-                            </Button>
                             <Button variant="outline" className="w-full">
                                 Login with Google
                             </Button>
+                            <div className="text-sm text-center">
+                                계정이 없으신가요?
+                                <Link to={"/sign-up"} className="underline ml-1">
+                                    회원가입
+                                </Link>
+                            </div>
                         </CardFooter>
                     </Card>
                 </div>
